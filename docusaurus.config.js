9fad6e92a604e09799cbaa7041d3ab2d5e9a54c2
Ruben van der Linde <ruben@conduction.nl>
2025-12-23 16:25:48 +0100

    fix: Update baseUrl for custom domain www.rubenlinde.nl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,10 +12,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://rubenvdlinde.github.io',
+  url: 'https://www.rubenlinde.nl',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/rubenlinde/',
+  // For custom domain, it should be '/'
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
